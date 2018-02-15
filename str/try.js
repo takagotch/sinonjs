d505c16ac5bec8e5876ae85ee81bb24c5e493dd4
takagotch <dyaccb@gmail.com>
2018-02-16 05:21:34 +0900

    called = true; returnValue = fn.apply(this, arguments);

---
+++ str/try.js
@@ -0,0 +1,13 @@
+function once(fn){
+  var returnValue, called = false;
+  return function(){
+    if(!called){
+      called = true;
+      returnValue = fn.apply(this, arguments);
+    }
+    return returnValue;
+  };
+}
+
+//
+
